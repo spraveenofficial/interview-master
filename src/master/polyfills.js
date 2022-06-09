22618ceee5cb033b76ce6b65c72a7595c51a0e4b
Praveen <spraveen593@gmail.com>
2022-06-10 01:26:25 +0530

    first commit

---
+++ src/master/polyfills.js
@@ -0,0 +1,62 @@
+const promise = new Promise((resolve, reject) => {
+  console.log("1d")
+  resolve('resilve')
+})
+
+const promise1 = promise.then((res) => {
+  console.log(res)
+})
+
+console.log("1" + " " + promise)
+console.log("2" + " " + promise1)
+
+Function.prototype.myBind = function (functions){
+    const obj = this;
+    return function() {
+      obj.call(functions)
+    }
+}
+
+// polyfill for map
+
+Array.prototype.myMap = function (cb){
+  var output = [];
+  for(var i = 0; i<this.length; i++){
+    output.push(cb(this[i], i , this))
+  }
+  return output
+}
+
+
+// Filter polyfills
+
+Array.prototype.myFilter = function (cb){
+  var output = [];
+  for(var i = 0; i< this.length; i++){
+    if(cb.call(this, this[i], i, this )){
+      output.push(this[i])
+    }
+  }
+  return output
+}
+
+// Bind pollyfill
+
+Function.prototype.myBind1 = function (functions) {
+  const obj = this;
+  return function(){
+    obj.call(functions)
+  }
+}
+
+// Filter polyfills
+
+Array.prototype.myFilter1 = function(objects){
+  var output = [];
+  for(var i=0; i<this.length; i++){
+    if(objects.call(this, this[i], i, this)){
+      output.push(this[i])
+    }
+  }
+  return output
+}
