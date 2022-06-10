20e66126fbf43f5cf4fc5a72d026a9b3ccbcbca9
Praveen <spraveen593@gmail.com>
2022-06-10 11:52:40 +0530

    Added: Bind Pollyfill

--- src/master/polyfills.js
+++ src/master/polyfills.js
@@ -1,62 +1,73 @@
 const promise = new Promise((resolve, reject) => {
-  console.log("1d")
-  resolve('resilve')
-})
+  console.log("1d");
+  resolve("resilve");
+});
 
 const promise1 = promise.then((res) => {
-  console.log(res)
-})
+  console.log(res);
+});
 
-console.log("1" + " " + promise)
-console.log("2" + " " + promise1)
+console.log("1" + " " + promise);
+console.log("2" + " " + promise1);
 
-Function.prototype.myBind = function (functions){
-    const obj = this;
-    return function() {
-      obj.call(functions)
-    }
+// Debounce polyfill
+
+function debounce(delay, cb) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
 }
+
+Function.prototype.myBind = function (functions) {
+  const obj = this;
+  return function () {
+    obj.call(functions);
+  };
+};
 
 // polyfill for map
 
-Array.prototype.myMap = function (cb){
+Array.prototype.myMap = function (cb) {
   var output = [];
-  for(var i = 0; i<this.length; i++){
-    output.push(cb(this[i], i , this))
+  for (var i = 0; i < this.length; i++) {
+    output.push(cb(this[i], i, this));
   }
-  return output
-}
-
+  return output;
+};
 
 // Filter polyfills
 
-Array.prototype.myFilter = function (cb){
+Array.prototype.myFilter = function (cb) {
   var output = [];
-  for(var i = 0; i< this.length; i++){
-    if(cb.call(this, this[i], i, this )){
-      output.push(this[i])
+  for (var i = 0; i < this.length; i++) {
+    if (cb.call(this, this[i], i, this)) {
+      output.push(this[i]);
     }
   }
-  return output
-}
+  return output;
+};
 
 // Bind pollyfill
 
 Function.prototype.myBind1 = function (functions) {
   const obj = this;
-  return function(){
-    obj.call(functions)
-  }
-}
+  return function () {
+    obj.call(functions);
+  };
+};
 
 // Filter polyfills
 
-Array.prototype.myFilter1 = function(objects){
+Array.prototype.myFilter1 = function (objects) {
   var output = [];
-  for(var i=0; i<this.length; i++){
-    if(objects.call(this, this[i], i, this)){
-      output.push(this[i])
+  for (var i = 0; i < this.length; i++) {
+    if (objects.call(this, this[i], i, this)) {
+      output.push(this[i]);
     }
   }
-  return output
-}
+  return output;
+};
